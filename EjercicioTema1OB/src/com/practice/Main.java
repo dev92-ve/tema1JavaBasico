c4b4146e74c6414830c6e6c570d0934571c0479a
dev92-ve <121239299+dev92-ve@users.noreply.github.com>
2023-01-23 22:04:35 -0400

    Add files via upload

---
+++ EjercicioTema1OB/src/com/practice/Main.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+public class Main {
+    public static void main(String[] args){
+        byte numero = 1;
+        System.out.println(numero);
+        int numero1 = 5;
+        System.out.println(numero1);
+        long numero2 = 5L;
+        System.out.println(numero2);
+        float numero3 = 5.5f;
+        System.out.println(numero3);
+        double numero4 = 5.5;
+        System.out.println(numero4);
+        boolean verdaderoFalso = true;
+        System.out.println(verdaderoFalso);
+        char letra = 'a';
+        System.out.println(letra);
+        String cadena = "Estos son todos los tipos de datos";
+        System.out.println(cadena);
+    }
+}
